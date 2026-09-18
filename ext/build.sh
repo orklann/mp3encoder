116b892e05dab026a048fe1621b6aa505dd85bdd
Tommy Jeff <threcius@yahoo.com>
2026-09-18 10:11:44 +0800

    Add lame-4.0

--- ext/build.sh
+++ ext/build.sh
@@ -1,14 +1,42 @@
 #!/bin/sh
 set -e
 
-mkdir -p lib
+EXT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
+LAME_DIR="$EXT_DIR/lame-4.0"
+DIST_DIR="$EXT_DIR/lib"
+
+mkdir -p "$DIST_DIR"
+
+echo "==> Configuring LAME"
+
+cd "$LAME_DIR"
+
+if [ ! -f Makefile ]; then
+  ./configure \
+    --disable-shared \
+    --enable-static \
+    --disable-frontend \
+    --disable-decoder
+fi
+
+echo "==> Building LAME"
+
+make -j"$(sysctl -n hw.ncpu)"
+
+echo "==> Building mp3encoder"
 
 clang \
-  -c \
   -O2 \
-  -o lib/libmp3encoder.o \
-  ext/mp3encoder.c
+  -c \
+  "$EXT_DIR/mp3encoder.c" \
+  -I"$LAME_DIR/include" \
+  -o "$DIST_DIR/mp3encoder.o"
+
+echo "==> Creating libmp3encoder.a"
 
 ar rcs \
-  lib/libmp3encoder.a \
-  lib/libmp3encoder.o
+  "$DIST_DIR/libmp3encoder.a" \
+  "$DIST_DIR/mp3encoder.o"
+
+echo "==> Done"
+echo "    $DIST_DIR/libmp3encoder.a"
